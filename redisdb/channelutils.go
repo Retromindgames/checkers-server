23247f8945585e69ce41f5e8033ab9f594a64896
Lavizord <63145101+Lavizord@users.noreply.github.com>
2025-02-16 17:24:06 +0000

    chore: GeneratePlayerRedisKey implemented

--- redisdb/channelutils.go
+++ redisdb/channelutils.go
@@ -5,6 +5,9 @@ import "checkers-server/models"
 func GetPlayerPubSubChannel(player models.Player) (string) {
 	return "player:"+string(player.ID)
 }
+func GeneratePlayerRedisKey(player models.Player) (string) {
+	return GetPlayerPubSubChannel(player)
+}
 func GenerateRoomRedisKey(room models.Room) (string) {
 	return "room:"+string(room.ID)
 }
--- roomworker/main.go
+++ roomworker/main.go
@@ -27,7 +27,7 @@ func init() {
 }
 
 func main() {
-	fmt.Printf("[Worker-%d] - Waiting for room messages...\n", pid)
+	fmt.Printf("[RoomWorker-%d] - Waiting for room messages...\n", pid)
 	go processRoomCreation()
 	go processRoomJoin()
 	go processRoomEnding()
@@ -38,10 +38,10 @@ func processRoomCreation(){
 	for {
 		playerData, err := redisClient.BLPop("create_room", 0) // Block
 		if err != nil {
-			fmt.Printf("[Worker-%d] - Error retrieving player:%v\n", pid, err)
+			fmt.Printf("[RoomWorker-%d] - Error retrieving player:%v\n", pid, err)
 			continue
 		}
-		fmt.Printf("[Worker-%d] - create room!: %+v\n", pid, playerData)
+		fmt.Printf("[RoomWorker-%d] - create room!: %+v\n", pid, playerData)
 		handleCreateRoom(playerData)
 	}
 }
@@ -50,10 +50,10 @@ func processRoomJoin(){
 	for {
 		playerData, err := redisClient.BLPop("join_room", 0) // Block
 		if err != nil {
-			fmt.Printf("[Worker-%d] - Error retrieving player:%v\n", pid, err)
+			fmt.Printf("[RoomWorker-%d] - Error retrieving player:%v\n", pid, err)
 			continue
 		}
-		fmt.Printf("[Worker-%d] - processing join room!: %+v\n", pid, playerData)
+		fmt.Printf("[RoomWorker-%d] - processing join room!: %+v\n", pid, playerData)
 		handleJoinRoom(playerData)
 	}
 }
@@ -63,19 +63,19 @@ func processRoomEnding(){
 	for {
 		playerData, err := redisClient.BLPop("end_room", 0) // Block
 		if err != nil {
-			fmt.Printf("[Worker-%d] - Error retrieving player:%v\n", pid, err)
+			fmt.Printf("[RoomWorker-%d] - Error retrieving player:%v\n", pid, err)
 			continue
 		}
-		fmt.Printf("[Worker-%d] - end room!: %+v\n", pid,playerData)
+		fmt.Printf("[RoomWorker-%d] - end room!: %+v\n", pid,playerData)
 	}
 }
 
 func handleCreateRoom(player *models.Player) {
-	fmt.Printf("[Worker-%d] - Handling player (CREATE ROOM): %s (Session: %s, Currency: %s)\n",
+	fmt.Printf("[RoomWorker-%d] - Handling player (CREATE ROOM): %s (Session: %s, Currency: %s)\n",
 		pid, player.ID, player.SessionID, player.Currency)
 
 	room := &models.Room{
-		ID:    models.GenerateUUID(),
+		ID:        models.GenerateUUID(),
 		Player1:   player,
 		StartDate: time.Now(),
 		Currency:  player.Currency,
@@ -83,34 +83,44 @@ func handleCreateRoom(player *models.Player) {
 	}
 	err := redisClient.AddRoom2(room)
 	if err != nil {
-		fmt.Printf("[Worker-%d] - Failed to add room to Redis: %v\n", pid, err)
+		fmt.Printf("[RoomWorker-%d] - Failed to add room to Redis: %v\n", pid, err)
 		return
 	}
+	player.RoomID = room.ID
+	redisClient.AddPlayer(redisdb.GeneratePlayerRedisKey(*player), player)		// This should update out player room info.
 	roomValue := models.RoomValue{
 		ID:          room.ID,
 		Player:      room.Player1.Name,
 		Currency:    room.Currency,
 		SelectedBid: room.BidAmount,
 	}
-	messageJson := &models.CreateRoomMessage{
-		Command: "room_created",
-		Value: roomValue,
+	messageJson := &messages.Message[models.RoomValue]{
+		Command: "room_created", 
+		Value:   roomValue,      
 	}
+	// Marshal the message into JSON
 	messageBytes, err := json.Marshal(messageJson)
 	if err != nil {
-		fmt.Printf("[Worker-%d] - Error marshalling message: %v\n", pid, err)
+		fmt.Printf("[RoomWorker-%d] - Error marshalling message: %v\n", pid, err)
 		return
 	}
+	_, err = messages.ParseMessage(messageBytes) // Will validate if the command is correct
+	if err != nil {
+		fmt.Printf("[RoomWorker-%d] - Invalid message format: %v\n", pid, err)
+		return
+	}
+	// Publish the validated message to Redis
 	err = redisClient.PublishToPlayer(*player, string(messageBytes))
 	if err != nil {
-		fmt.Printf("[Worker-%d] - Failed to publish message to player: %v\n", pid, err)
+		fmt.Printf("[RoomWorker-%d] - Failed to publish message to player: %v\n", pid, err)
 		return
 	}
-	fmt.Printf("[Worker-%d] - Player successfully handled and notified, Room ID: %s\n", pid, room.ID)
+	fmt.Printf("[RoomWorker-%d] - Player successfully handled and notified, Room ID: %s\n", pid, room.ID)
 }
 
+
 func handleJoinRoom(player *models.Player) {
-	fmt.Printf("[Worker-%d] - Handling player (JOIN ROOM): %s (Session: %s, Currency: %s)\n",
+	fmt.Printf("[RoomWorker-%d] - Handling player (JOIN ROOM): %s (Session: %s, Currency: %s)\n",
 		pid, player.ID, player.SessionID, player.Currency)
 	rooms, err := redisClient.GetEmptyRoomsByBidAmount(player.SelectedBid)
 	if err != nil {
@@ -119,6 +129,7 @@ func handleJoinRoom(player *models.Player) {
 	message, err := messages.GeneratePairedMessage(rooms[0].Player1, player)
 	redisClient.PublishPlayerEvent(rooms[0].Player1, message)
 	redisClient.PublishPlayerEvent(player, message)
+
 }
 
 
