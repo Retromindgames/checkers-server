d0b6c2a6cd327ebb058d37a1b39a62274d402242
Lavizord <63145101+Lavizord@users.noreply.github.com>
2025-02-16 21:25:48 +0000

    feat: detection and deleting of room / aggregate on player disconnect

--- pstatusworker/main.go
+++ pstatusworker/main.go
@@ -25,7 +25,7 @@ func init() {
 }
 
 func main() {
-	fmt.Printf("[Worker-%d] - Waiting for player connections...\n", pid)
+	fmt.Printf("[PStatus Worker-%d] - Waiting for player connections...\n", pid)
 	go processPlayerOnline()
 	go processPlayerOffline()
 	go processPlayerUpdate()
@@ -37,22 +37,23 @@ func processPlayerOnline(){
 	for {
 		playerData, err := redisClient.BLPop("player_online", 0) // Block
 		if err != nil {
-			fmt.Printf("[Worker-%d] - Error retrieving player:%v\n", pid, err)
+			fmt.Printf("[PStatus Worker-%d] - Error retrieving player:%v\n", pid, err)
 			continue
 		}
-		fmt.Printf("[Worker-%d] - Player connected: %+v\n", pid, playerData)
+		fmt.Printf("[PStatus Worker-%d] - Player connected: %+v\n", pid, playerData)
 		handleNewPlayer(playerData)
 	}
 }
 
 func processPlayerUpdate(){
 	for {
-		playerData, err := redisClient.BLPop("player_update", 0) // Block
+		message, err := redisClient.BLPop("player_update", 0) // Block
 		if err != nil {
-			fmt.Printf("[Worker-%d] - Error retrieving player:%v\n", pid, err)
+			fmt.Printf("[PStatus Worker-%d] - Error retrieving update message:%v\n", pid, err)
 			continue
 		}
-		updatePlayer(playerData)
+		fmt.Printf("[PStatus Worker-%d] - TODO Processing player update: %+v\n", pid, message)
+		//updatePlayerToRedis(playerData)
 	}
 }
 
@@ -60,61 +61,61 @@ func processPlayerOffline(){
 	for {
 		playerData, err := redisClient.BLPop("player_offline", 0) // Block
 		if err != nil {
-			fmt.Printf("[Worker-%d] - Error retrieving player: %d\n", pid, err)
+			fmt.Printf("[PStatus Worker-%d] - Error retrieving player: %d\n", pid, err)
 			continue
 		}
-		fmt.Printf("[Worker-%d] - Player disconnected: %+v\n", pid, playerData)
+		fmt.Printf("[PStatus Worker-%d] - Player disconnected: %+v\n", pid, playerData)
 		handleRemovePlayer(playerData)
 	}
 }
 
 func handleRemovePlayer(player *models.Player) {
-	fmt.Printf("[Worker-%d] - Removing player: %s (Session: %s, Currency: %s)\n",
-		pid, player.ID, player.SessionID, player.Currency)
+	fmt.Printf("[PStatus Worker-%d] - Removing player: %s (Session: %s, Currency: %s, RoomID: %s)\n",
+		pid, player.ID, player.SessionID, player.Currency, player.RoomID)
+
+	if(player.RoomID != ""){
+		fmt.Printf("[PStatus Worker-%d] - Removed player is in a Room, sending notification to room worker!: %v\n", pid, player)
+		redisClient.RPush("leave_room", player)
+	}
 
 	err := redisClient.RemovePlayer(string(player.ID))
 	if err != nil {
-		fmt.Printf("[Worker-%d] - Failed to remove player: %v\n", pid, err)
+		fmt.Printf("[PStatus Worker-%d] - Failed to remove player: %v\n", pid, err)
 		return
 	}
-	err = redisClient.PublishToPlayer(*player, "player-offline")
-	if err != nil {
-		fmt.Printf("[Worker-%d] - Failed to publish player-offline event: %v\n", pid, err)
-		return
-	}
-	fmt.Printf("[Worker-%d] - Player successfully removed and notified.\n", pid)
+	fmt.Printf("[PStatus Worker-%d] - Player successfully removed.\n", pid)
 }
 
 func handleNewPlayer(player *models.Player) {
-	fmt.Printf("[Worker-%d] - Handling player: %s (Session: %s, Currency: %s)\n",
+	fmt.Printf("[PStatus Worker-%d] - Handling player: %s (Session: %s, Currency: %s)\n",
 		pid, player.ID, player.SessionID, player.Currency)
 
 	err := redisClient.AddPlayer(player)
 	if err != nil {
-		fmt.Printf("[Worker-%d] - Failed to add player: %v\n", pid, err)
+		fmt.Printf("[PStatus Worker-%d] - Failed to add player: %v\n", pid, err)
 		return
 	}
 	jsonMessage, err := messages.GenerateConnectedMessage(player)
 	err = redisClient.PublishToPlayer(*player, jsonMessage)
 	if err != nil {
-		fmt.Printf("[Worker-%d] - Failed to publish message to player: %v\n", pid, err)
+		fmt.Printf("[PStatus Worker-%d] - Failed to publish message to player: %v\n", pid, err)
 		return
 	}
-	fmt.Printf("[Worker-%d] - Player successfully handled and notified.\n", pid)
+	fmt.Printf("[PStatus Worker-%d] - Player successfully handled and notified.\n", pid)
 }
 
-func updatePlayer(player *models.Player) {
+func updatePlayerToRedis(player *models.Player) {
 	
 	err := redisClient.AddPlayer(player)
 	if err != nil {
-		fmt.Printf("[Worker-%d] - Failed to add player: %v\n", pid, err)
+		fmt.Printf("[PStatus Worker-%d] - Failed to add player: %v\n", pid, err)
 		return
 	}
 	jsonMessage, err := messages.GenerateConnectedMessage(player)
 	err = redisClient.PublishToPlayer(*player, jsonMessage)
 	if err != nil {
-		fmt.Printf("[Worker-%d] - Failed to publish message to player: %v\n", pid, err)
+		fmt.Printf("[PStatus Worker-%d] - Failed to publish message to player: %v\n", pid, err)
 		return
 	}
-	fmt.Printf("[Worker-%d] - Player successfully handled and notified.\n", pid)
+	fmt.Printf("[PStatus Worker-%d] - Player successfully handled and notified.\n", pid)
 }
