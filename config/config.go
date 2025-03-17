d01321f51d7b56cbaeaf2a5702a9cc79a82b267b
Lavizord <pedro.s.nunes94@gmail.com>
2025-03-17 08:06:29 +0000

    feat: config with postgres params

--- config/config.go
+++ config/config.go
@@ -30,13 +30,21 @@ type Config struct {
 		Addr string `json:"addr"`
 		DB   int    `json:"db"`
 	} `json:"redis"`
+	Postgres struct {
+		User     string `json:"user"`
+		Password string `json:"password"`
+		DBName   string `json:"dbname"`
+		Host     string `json:"host"`
+		Port     string `json:"port"`
+	} `json:"postgres"`
 	Services map[string]struct {
-		Ports          []int  `json:"ports,omitempty"`
-		Timer          int    `json:"timer,omitempty"`
-		TimerSetting   string `json:"timer_setting,omitempty"` // New field
-		PiecesInMatch  int    `json:"pieces_in_match,omitempty"` // New field
+		Ports         []int  `json:"ports,omitempty"`
+		Timer         int    `json:"timer,omitempty"`
+		TimerSetting  string `json:"timer_setting,omitempty"`
+		PiecesInMatch int    `json:"pieces_in_match,omitempty"`
 	} `json:"services"`
 }
+
 // Global config instance
 var Cfg Config
 
@@ -44,12 +52,12 @@ func LoadConfig() {
 	configPath := os.Getenv("CONFIG_PATH")
 	if configPath == "" {
 		log.Fatal("[config.go] - CONFIG_PATH not set")
-	} 
+	}
 	log.Println("[config.go] - Config path is:", configPath)
-	
+
 	file, err := os.Open(configPath)
 	if err != nil {
-		log.Fatalf("[config.go] -Error opening config file: %v", err)
+		log.Fatalf("[config.go] - Error opening config file: %v", err)
 	}
 	defer file.Close()
 
@@ -57,5 +65,5 @@ func LoadConfig() {
 	if err := decoder.Decode(&Cfg); err != nil {
 		log.Fatalf("[config.go] - Error decoding JSON: %v", err)
 	}
-	log.Printf("[config.go] -Config loaded: %+v", Cfg)
+	log.Printf("[config.go] - Config loaded: %+v", Cfg)
 }
