90ef695c52b08911ccf289489e407c5a138a421a
Lavizord <pedro.s.nunes94@gmail.com>
2025-04-30 21:45:49 +0100

    chore: wsapi healthcheck

--- main.go
+++ main.go
@@ -10,13 +10,8 @@ import (
 	"os"
 )
 
-func main() {
-
+func init() {
 	config.LoadConfig()
-	ports := config.Cfg.Services["wsapi"].Ports
-	if len(ports) == 0 {
-		log.Fatal("[wsapi] - No ports defined for wsapi\n")
-	}
 	redisConData := config.Cfg.Redis
 	client, err := redisdb.NewRedisClient(redisConData.Addr, redisConData.User, redisConData.Password)
 	if err != nil {
@@ -24,28 +19,41 @@ func main() {
 	}
 	wsapi.RedisClient = client
 
-	// Get SSL cert paths from env
+}
+
+func hasLocalCerts() (exists bool, cert string, key string) {
 	certPath := os.Getenv("SSL_CERT_PATH")
 	keyPath := os.Getenv("SSL_KEY_PATH")
+	if certPath == "" || keyPath == "" {
+		return false, "", ""
+	}
+	return true, certPath, keyPath
+}
 
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+func main() {
+
+	port := config.FirstPortFromConfig("wsapi")
+	addrs := fmt.Sprintf(":%d", port)
+
+	http.HandleFunc("/ws/health", func(w http.ResponseWriter, r *http.Request) {
+		if err := wsapi.RedisClient.Client.Ping(r.Context()).Err(); err != nil {
+			http.Error(w, "Redis unavailable", http.StatusServiceUnavailable)
+			return
+		}
 		w.WriteHeader(http.StatusOK)
 	})
-	go wsapi.SubscribeToBroadcastChannel() // This is a global channel. WSAPI will send the messages from this channel to all active ws connections
 
-	if certPath == "" || keyPath == "" {
-		port := ports[0] // First port for HTTP
-		addr := fmt.Sprintf(":%d", port)
-		http.HandleFunc("/ws", wsapi.HandleConnection)
-		log.Println("[wsapi] - SSL certificate paths not set, defaulting to listen on HTTP.")
-		log.Printf("[wsapi] - WebSocket server started on %s\n", addr)
-		log.Fatal(http.ListenAndServe(addr, nil))
-	} else {
-		port := ports[1] // Second port for SSL
-		addr := fmt.Sprintf(":%d", port)
+	go wsapi.SubscribeToBroadcastChannel() // This is a global channel. WSAPI will send the messages from this channel to all active ws connections
+	has, certPath, keyPath := hasLocalCerts()
+	if has {
 		http.HandleFunc("/ws", wsapi.HandleConnection)
 		log.Println("[wsapi] - SSL certificate paths set, listening on HTTPS .")
-		log.Printf("[wsapi] - WebSocket server started on %s\n", addr)
-		log.Fatal(http.ListenAndServeTLS(addr, certPath, keyPath, nil))
+		log.Printf("[wsapi] - WebSocket server started on %s\n", addrs)
+		log.Fatal(http.ListenAndServeTLS(addrs, certPath, keyPath, nil))
+	} else {
+		http.HandleFunc("/ws", wsapi.HandleConnection)
+		log.Println("[wsapi] - SSL certificate paths not set, defaulting to listen on HTTP.")
+		log.Printf("[wsapi] - WebSocket server started on %s\n", addrs)
+		log.Fatal(http.ListenAndServe(addrs, nil))
 	}
 }
