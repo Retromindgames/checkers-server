6298634db38e81d587bec9c3050d7a255adc5658
Lavizord <pedro.s.nunes94@gmail.com>
2025-09-09 20:26:12 +0100

    feat: chess board state updating after each move.

--- gameworkers/chessworker/chessworker.go
+++ gameworkers/chessworker/chessworker.go
@@ -1,6 +1,8 @@
 package chessworker
 
 import (
+	"fmt"
+
 	"github.com/Lavizord/checkers-server/gameworkers/gameworker"
 	"github.com/Lavizord/checkers-server/logger"
 	"github.com/Lavizord/checkers-server/messages"
@@ -30,17 +32,26 @@ func (cw *ChessWorker) ProcessGameMovesList() {
 }
 
 func (cw *ChessWorker) HandleMove(game *models.Game, move models.MoveInterface, player *models.Player, piece models.PieceInterface) error {
+	// Since the move was validated and passed to the other player, its time to check for our end turn / end game conditions.
+	// This means we can add the move to our game.
+	game.Moves = append(game.Moves, move)
+	game.Board.MovePiece(move.GetFrom(), move.GetTo()) // This should handle the capture as well, since it overwrites the destination position.
+	game.UpdatePlayerPieces()
 	msg, err := messages.GenerateMoveMessage(move)
 	if err != nil {
 		logger.Default.Errorf("(Process Game Moves) - failed to generate move message: %+v, from game with id: %v, from player with id: %v", move, player.GameID, move.GetPlayerID())
 	}
-	//log.Printf("[%s-%d] - (Process Game Moves) - Message to publish: %v\n", name, pid, string(msg))
 	opponent, _ := game.GetOpponentGamePlayer(move.GetPlayerID())
 	cw.RedisClient.PublishToGamePlayer(*opponent, string(msg))
+
+	// We check for game Over
+	if game.CheckGameOver() {
+		msg := fmt.Sprintf("(Process Game Moves) - determined game is over, from game with id: %v, from player with id: %v", player.GameID, move.GetPlayerID())
+		logger.Default.Infof(msg)
+		cw.HandleGameEnd(game, "winner", move.GetPlayerID())
+		return nil
+	}
 	cw.HandleTurnChange(game)
-	// Since the move was validated and passed to the other player, its time to check for our end turn / end game conditions.
-	// This means we can add the move to our game.
-	game.Moves = append(game.Moves, move)
 
 	return nil
 }
